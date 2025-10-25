39371ce9828af1be8b1527d97716b9749f1aa77c
Egor <arnoldtagirov1@gmail.com>
2025-10-25 20:43:42 +0200

    -M

---
+++ MagazineAndBooks/MagazineAndBooks/Domain/Entities/Magazine.cs
@@ -0,0 +1,50 @@
+﻿namespace MagazineAndBooks.Domain.Entities
+{
+    public class Magazine
+    {
+        public required string Name { get; set; }
+
+        public required int YearOfFoundation { get; set; }
+
+        public required  string Description { get; set; }
+
+        public required string EMail { get; set; }
+
+        public int CountOfWorkers { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name {Name}\nYearOfFoundation {YearOfFoundation}\nDescription {Description}\nEmail {EMail}");
+        }
+
+        public static Magazine operator+(Magazine a, int count)
+        {
+            a.CountOfWorkers += count;
+            return a;
+        }
+        public static Magazine operator -(Magazine a, int count)
+        {
+            a.CountOfWorkers -= count;
+            return a;
+        }
+        public static bool operator ==(Magazine a, Magazine b)
+        {
+            return a.CountOfWorkers == b.CountOfWorkers;
+        }
+        public static bool operator !=(Magazine a, Magazine b)
+        {
+            return a.CountOfWorkers != b.CountOfWorkers;
+        }
+
+        public static bool operator >(Magazine a, Magazine b)
+        {
+            return a.CountOfWorkers > b.CountOfWorkers;
+        }
+
+        public static bool operator <(Magazine a, Magazine b)
+        {
+            return a.CountOfWorkers < b.CountOfWorkers;
+        }
+
+    }
+}
