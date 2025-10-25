1d44656cfbab9730e979a71a3e6d96be9c55eda2
Egor <arnoldtagirov1@gmail.com>
2025-10-25 20:52:38 +0200

    -m

---
+++ MagazineAndBooks/MagazineAndBooks/Domain/Entities/Shop.cs
@@ -0,0 +1,37 @@
+﻿namespace MagazineAndBooks.Domain.Entities
+{
+    public class Shop
+    {
+        public required string Name {  get; set; }
+        public required double ShopArea {  get; set; }
+
+        public static Shop operator +(Shop a, double count)
+        {
+            a.ShopArea += count;
+            return a;
+        }
+        public static Shop operator -(Shop a, double count)
+        {
+            a.ShopArea -= count;
+            return a;
+        }
+
+        public static bool operator ==(Shop a, Shop b)
+        {
+            return a.ShopArea == b.ShopArea;
+        }
+        public static bool operator !=(Shop a, Shop b)
+        {
+            return a.ShopArea != b.ShopArea;
+        }
+        public static bool operator >(Shop a, Shop b)
+        {
+            return a.ShopArea > b.ShopArea;
+        }
+        public static bool operator <(Shop a, Shop b)
+        {
+            return a.ShopArea < b.ShopArea;
+        }
+
+    }
+}
