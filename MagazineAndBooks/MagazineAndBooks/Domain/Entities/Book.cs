2c18b1a6a3f2a947b1cccfcf4852260e056f21ac
Egor <arnoldtagirov1@gmail.com>
2025-10-25 21:18:00 +0200

    -m

---
+++ MagazineAndBooks/MagazineAndBooks/Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace MagazineAndBooks.Domain.Entities
+{
+    public class Book
+    {
+        public required string BookName {  get; set; }
+
+        public required string Author {  get; set; }
+
+        public required int CountOfPages { get; set; }
+        public decimal Price { get; set; }
+
+        public void ShowBookInfo()
+        {
+            Console.WriteLine($"Name of the book {BookName}\nAuthor {Author}\nCount of Pages {CountOfPages}");
+        }
+        public void BookPrice()
+        {
+            Console.WriteLine($"Price {Price}");
+        }
+    }
+}
---
+++ MagazineAndBooks/MagazineAndBooks/Domain/Entities/BooksToRead.cs
@@ -0,0 +1,39 @@
+﻿namespace MagazineAndBooks.Domain.Entities
+{
+    public class Library
+    {
+        public required string Name {  get; set; }
+        public required List<Book> Books { get; set; }
+
+        public int CountOfBooks()
+        {
+            return Books.Count;
+        }
+
+        public bool IsBookInLibrary(Book book)
+        {
+
+            foreach (var item in Books)
+            {
+                if (item == book)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public static Library operator +(Book book,Library a)
+        {
+            a.Books.Add(book);
+            return a;
+        }
+        public static Library operator -(Book book, Library a)
+        {
+            
+            a.Books.Remove(book);
+            return a;
+        }
+    }
+}
