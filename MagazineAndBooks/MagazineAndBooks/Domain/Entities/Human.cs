20bb2fb3a27397871cd0a68a84a32835397ebee9
Egor <arnoldtagirov1@gmail.com>
2025-10-26 15:09:56 +0100

    -m

---
+++ MagazineAndBooks/MagazineAndBooks/Domain/Entities/Human.cs
@@ -0,0 +1,17 @@
+﻿namespace MagazineAndBooks.Domain.Entities
+{
+    public class Human // небольшой класс для задачки с работником
+    {
+        public required string Name { get; set; }
+
+        public  required string Surname { get; set; }
+
+        public required int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name {Name}\nSurname {Surname}\nAge {Age}";
+        }
+
+    }
+}
---
+++ MagazineAndBooks/MagazineAndBooks/Domain/Entities/Worker.cs
@@ -0,0 +1,58 @@
+﻿namespace MagazineAndBooks.Domain.Entities
+{
+    public class Worker:Human
+    {
+        public required decimal Salary  { get; set; }
+
+        public required string JobTitle { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nJob title {JobTitle}\nSalary {Salary}\n";
+        }
+        public static Worker operator+(Worker w,decimal s)
+        {
+            w.Salary += s ;
+            return w ;
+        }
+        public static Worker operator -(Worker w, decimal s)
+        { 
+            w.Salary -= s;
+            return w;
+
+
+        }
+        public static bool operator ==(Worker w, Worker s)
+        {
+            if (ReferenceEquals(w, null) && ReferenceEquals(s, null)) return true;
+            if (ReferenceEquals(w, null) || ReferenceEquals(s, null)) return false;
+            return w.Salary == s.Salary;
+
+        }
+        public static bool operator !=(Worker w, Worker s)
+        {
+           return !(w.Salary == s.Salary);
+
+        }
+        public static bool operator >(Worker w, Worker s)
+        {
+            return w.Salary > s.Salary;
+
+        }
+        public static bool operator <(Worker w, Worker s)
+        {
+            return w.Salary < s.Salary;
+        }
+        public override int GetHashCode()
+        {
+            return Salary.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            Worker other = (Worker)obj;
+            return Salary == other.Salary;
+        }
+
+    }
+}
